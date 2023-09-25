84d528266cfa7bf513f204cca6d1dcbfa382b68e
GWSG <144191410+GWSG@users.noreply.github.com>
2023-09-25 16:10:16 +0800

    Update MainWindow.xaml.cs
    更新完成

--- WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
 
 namespace WpfApp2
 {
@@ -12,11 +14,58 @@ namespace WpfApp2
     {
         Dictionary<string, int> drinks = new Dictionary<string, int>();
         Dictionary<string, int> orders = new Dictionary<string, int>();
+        string takeout = "";
         public MainWindow()
         {
             InitializeComponent();
+
             //新增所有飲料品項
             AddNewDrink(drinks);
+
+            //顯示飲料品項菜單
+            DisplayDrinkMenu(drinks);
+        }
+
+        private void DisplayDrinkMenu(Dictionary<string, int> myDrinks)
+        {
+            foreach (var drink in myDrinks)
+            {
+                StackPanel sp = new StackPanel();
+                sp.Orientation = Orientation.Horizontal;
+
+                CheckBox cb = new CheckBox();
+                cb.Content = $"{drink.Key} : {drink.Value}元";
+                cb.Width = 200;
+                cb.FontFamily = new FontFamily("Consolas");
+                cb.FontSize = 18;
+                cb.Foreground = Brushes.Blue;
+                cb.Margin = new Thickness(5);
+
+                Slider sl = new Slider();
+                sl.Width = 100;
+                sl.Value = 0;
+                sl.Minimum = 0;
+                sl.Maximum = 10;
+                sl.IsSnapToTickEnabled = true;
+
+                Label lb = new Label();
+                lb.Width = 50;
+                lb.Content = "0";
+                lb.FontFamily = new FontFamily("Consolas");
+                lb.FontSize = 18;
+                lb.Foreground = Brushes.Red;
+
+                sp.Children.Add(cb);
+                sp.Children.Add(sl);
+                sp.Children.Add(lb);
+
+                //資料繫結
+                Binding myBinding = new Binding("Value");
+                myBinding.Source = sl;
+                lb.SetBinding(ContentProperty, myBinding);
+
+                stackpanel_DrinkMenu.Children.Add(sp);
+            }
         }
 
         private void AddNewDrink(Dictionary<string, int> myDrinks)
@@ -27,31 +76,18 @@ namespace WpfApp2
             myDrinks.Add("綠茶小杯", 40);
             myDrinks.Add("咖啡大杯", 80);
             myDrinks.Add("咖啡小杯", 50);
-        }
-
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            TextBox targetTextBox = sender as TextBox;
-
-            bool success = int.TryParse(targetTextBox.Text, out int amount);
-
-            if (!success) MessageBox.Show("請輸入整數", "輸入錯誤");
-            else if (amount <= 0) MessageBox.Show("請輸入正整數", "輸入錯誤");
-            else
-            {
-                StackPanel targetStackPanel = targetTextBox.Parent as StackPanel;
-                Label targetLabel = targetStackPanel.Children[0] as Label;
-                string drinkName = targetLabel.Content.ToString();
-                if (orders.ContainsKey(drinkName)) orders.Remove(drinkName);
-                orders.Add(drinkName, amount);
-            }
+            myDrinks.Add("可樂大杯", 40);
+            myDrinks.Add("可樂小杯", 20);
         }
 
         private void OrderButton_Click(object sender, RoutedEventArgs e)
         {
+            //將訂購的飲料加入訂單
+            PlaceOrder(orders);
+
             double total = 0.0;
             double sellPrice = 0.0;
-            string displayString = "訂購清單如下：\n";
+            string displayString = $"本次訂購為{takeout}，清單如下：\n";
             string message = "";
 
             foreach (KeyValuePair<string, int> item in orders)
@@ -87,6 +123,31 @@ namespace WpfApp2
             displayString += $"本次訂購總共{orders.Count}項，{message}，售價{sellPrice}元";
 
             textblock1.Text = displayString;
+        }
+
+        private void PlaceOrder(Dictionary<string, int> myOrders)
+        {
+            myOrders.Clear();
+            for (int i = 0; i < stackpanel_DrinkMenu.Children.Count; i++)
+            {
+                StackPanel sp = stackpanel_DrinkMenu.Children[i] as StackPanel;
+                CheckBox cb = sp.Children[0] as CheckBox;
+                Slider sl = sp.Children[1] as Slider;
+                string drinkName = cb.Content.ToString().Substring(0, 4);
+                int quantity = Convert.ToInt32(sl.Value);
+
+                if (cb.IsChecked == true && quantity != 0)
+                {
+                    myOrders.Add(drinkName, quantity);
+                }
+            }
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            var rb = sender as RadioButton;
+            if (rb.IsChecked == true) takeout = rb.Content.ToString();
+            //MessageBox.Show(takeout);
         }
     }
 }
