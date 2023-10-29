a545757e1ac8dd4256330d9fa9dda0b1f2621260
GWSG <144191410+GWSG@users.noreply.github.com>
2023-10-29 20:28:57 +0800

    Update MainWindow.xaml.cs
    kk

--- WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/MainWindow.xaml.cs
@@ -11,25 +11,28 @@ using System.Windows.Media; // 繪圖和顏色
 
 namespace WpfApp2// 命名空間
 {
-    public partial class MainWindow : Window // 主窗口類繼承自 Window
+    // 宣告 MainWindow 類別，該類別是 Window 的一個部分定義（partial class），通常用於XAML與程式碼分離的情況。
+    public partial class MainWindow : Window 
     {
-        // 字典，用於儲存飲料名稱和價格
+        // 宣告一個字典 'drinks' 用於儲存飲料名稱作為 key，價格作為 value。
         Dictionary<string, int> drinks = new Dictionary<string, int>();
 
-        // 字典，用於儲存訂單，包括飲料名稱和數量
+        // 宣告一個字典 'orders' 用於儲存訂單中飲料名稱作為 key，數量作為 value。
         Dictionary<string, int> orders = new Dictionary<string, int>();
 
-        // 用於儲存外帶選項的變數
+        // 宣告一個字串變數 'takeout'，用於儲存外帶選項。
         string takeout = "";
 
-        public MainWindow()// 構造函數
-        {
-            InitializeComponent();// 初始化XAML元素
 
-            // 將飲料項目添加到 'drinks' 字典中
+        // MainWindow 的建構子函數。
+        public MainWindow() 
+        {
+            InitializeComponent(); // 初始化XAML中的界面元素，這是自動生成的函數。
+
+            // 調用自定義的 AddNewDrink 函數，將 drinks 字典作為參數傳入。
             AddNewDrink(drinks);
 
-            // 在UI上顯示飲料菜單
+            // 調用自定義的 DisplayDrinkMenu 函數，將 drinks 字典作為參數傳入，以在UI上顯示飲料菜單。 
             DisplayDrinkMenu(drinks);
         }
 
@@ -79,36 +82,63 @@ namespace WpfApp2// 命名空間
                     Foreground = Brushes.Red// 設置文本的前景色為紅色。
                 };
 
-                // 將創建的 CheckBox、Slider 和 Label 控件添加到 StackPanel 的 Children 集合中
-                sp.Children.Add(cb);
-                sp.Children.Add(sl);
-                sp.Children.Add(lb);
+                
+                sp.Children.Add(cb);// 將 CheckBox 控件 (cb) 添加到 StackPanel (sp) 的 Children 
+                sp.Children.Add(sl);// 將 Slider 控件 (sl) 添加到 StackPanel (sp) 的 Children 
+                sp.Children.Add(lb);// 將 Label 控件 (lb) 添加到 StackPanel (sp) 的 Children 
 
-                // 數據綁定以將滑塊值和標籤內容關聯起來
+
+                // 創建一個新的 Binding 對象，其 Path 為 "Value"
+                // 這將會對應到 Slider 的 Value 屬性
                 Binding myBinding = new Binding("Value");
+
+                // 設定 Binding 對象的 Source 屬性為 Slider 控件 (sl)
                 myBinding.Source = sl;
+
+                // 將 Label 控件 (lb) 的 ContentProperty 與剛剛創建的 Binding 對象進行綁定
+                // 這將使 Label 的內容隨著 Slider 的 Value 屬性變化而變化
                 lb.SetBinding(ContentProperty, myBinding);
 
-                // 將StackPanel添加到主UI StackPanel
+
+                // 將整個 StackPanel (sp) 添加到主 UI 的 StackPanel (stackpanel_DrinkMenu) 的 Children 
+                // 這樣，剛剛建立的 CheckBox, Slider, 和 Label 將會顯示在 UI 上
                 stackpanel_DrinkMenu.Children.Add(sp);
             }
         }
 
-        // 從文件中添加新的飲料到 'drinks' 字典
+        // 定義一個名為AddNewDrink的私有方法，接收一個字典作為參數。
         private void AddNewDrink(Dictionary<string, int> myDrinks)
         {
-            OpenFileDialog openFileDialog = new OpenFileDialog();// 創建文件選擇對話框
-            openFileDialog.Filter = "CSV文件|*.csv|文本文件|*.txt|所有文件|*.*";// 設置對話框的文件過濾選項
-            if (openFileDialog.ShowDialog() == true)// 顯示對話框
+
+            // 創建一個OpenFileDialog對象用於打開文件對話框。
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            // 設定文件選擇對話框的過濾器，以便用戶只能選擇特定類型的文件。
+            openFileDialog.Filter = "CSV文件|*.csv|文本文件|*.txt|所有文件|*.*";
+
+            // 打開文件選擇對話框，並檢查用戶是否成功選擇了一個文件。
+            if (openFileDialog.ShowDialog() == true)
             {
-                string filename = openFileDialog.FileName;// 獲取選定文件的完整路徑
-                string[] lines = File.ReadAllLines(filename);// 讀取文件的所有行
-                foreach (var line in lines)// 遍歷每一行
+                // 獲取用戶選擇的文件名。
+                string filename = openFileDialog.FileName;
+
+                // 讀取文件中的所有行並存儲到一個字符串數組中。
+                string[] lines = File.ReadAllLines(filename);
+
+                // 遍歷每一行來分解飲料名稱和價格。
+                foreach (var line in lines)
                 {
-                    string[] tokens = line.Split(',');// 使用逗號分隔每一行
-                    string drinkName = tokens[0];// 提取飲料名稱和價格
-                    int price = Convert.ToInt32(tokens[1]); // 將價格從字符串轉為整數
-                    myDrinks.Add(drinkName, price);// 添加到 'drinks' 字典
+                    // 使用逗號來分割每一行，並將結果存儲到一個字符串數組中。
+                    string[] tokens = line.Split(',');
+
+                    // 取出分割後的第一個元素作為飲料名稱。
+                    string drinkName = tokens[0];
+
+                    // 取出分割後的第二個元素作為飲料價格，並將其轉換為整數。
+                    int price = Convert.ToInt32(tokens[1]);
+
+                    // 將飲料名稱和價格添加到 'drinks' 字典。
+                    myDrinks.Add(drinkName, price);
                 }
             }
         }
@@ -116,28 +146,41 @@ namespace WpfApp2// 命名空間
         // "訂購"按鈕點擊事件處理程序
         private void OrderButton_Click(object sender, RoutedEventArgs e)
         {
-            // 將選擇的飲料放入 'orders' 字典
+            // "PlaceOrder(orders)" 是一個自定義函數，它負責將選中的飲料添加到 'orders' 字典中。
+            // 'orders' 字典將儲存飲料名稱作為鍵，數量作為值。
             PlaceOrder(orders);
 
+            // "DisplayOrder(orders)" 是另一個自定義函數，它會讀取 'orders' 字典，
+            // 並在某個用戶界面元素（如一個列表框或標籤）上顯示訂單的詳細信息。
             // 在UI上顯示訂單詳細信息
             DisplayOrder(orders);
 
-            // 將訂單寫入文本文件
+
+            // "SaveOrderToFile(orders)" 是第三個自定義函數，它將 'orders' 字典中的訂單信息
+            // 寫入到一個文本文件或其他儲存格式中，以便日後查詢或處理。
             SaveOrderToFile(orders);
         }
 
         // 在UI上顯示訂單詳細信息
         private void DisplayOrder(Dictionary<string, int> myOrders)
         {
-            displayTextBlock.Inlines.Clear();// 清空TextBlock的內容
-            Run titleString = new Run // 創建用於顯示訂單標題的Run對象
+            // "Inlines" 屬性表示一個 TextBlock 內的所有內聯元素（比如文字、超連接等）的集合。
+            // "Clear()" 函數會移除集合中的所有元素，從而使 TextBlock 變為空白。
+            // 清空TextBlock的內容
+            displayTextBlock.Inlines.Clear();
+
+
+            // 創建用於顯示訂單標題的Run對象
+            Run titleString = new Run 
             {
                 Text = "您所訂購的飲品為 ",
                 FontSize = 16,
                 Foreground = Brushes.Blue
             };
 
-            Run takeoutString = new Run // 創建用於顯示外帶選項的Run對象
+
+            // 創建用於顯示外帶或內用選項的Run對象
+            Run takeoutString = new Run 
             {
                 Text = $"{takeout}",
                 FontSize = 16,// 設置字體大小為 16。
@@ -145,15 +188,22 @@ namespace WpfApp2// 命名空間
             };
 
             // 添加標題和外帶信息到顯示中
+            // 將名為 titleString 的字串添加到 displayTextBlock 的 Inlines 集合中。
+            // 這將作為訂單詳情的標題。
             displayTextBlock.Inlines.Add(titleString);// 將標題和外帶選項添加到TextBlock中
+
+            // 將名為 takeoutString 的字串添加到 displayTextBlock 的 Inlines 集合中。
+            // 這將顯示用戶是否選擇了外帶選項。
             displayTextBlock.Inlines.Add(takeoutString);
             displayTextBlock.Inlines.Add(new Run() { Text = " ，訂購明細如下: \n", FontSize = 16 });
 
             double total = 0.0;// 用於存儲總價格
             double sellPrice = 0.0; // 用於存儲售價
             string discountString = ""; // 用於存儲折扣信息
-
             int i = 1;
+
+
+            //飲料品項...飲料名稱...什麼杯...每杯幾元...總共幾元
             foreach (var item in myOrders)
             {
                 string drinkName = item.Key;// 獲取飲料名稱
@@ -196,9 +246,11 @@ namespace WpfApp2// 命名空間
                 FontWeight = FontWeights.Bold, // 設置字體權重為 Bold，使文本看起來更加粗體。
                 Foreground = Brushes.Red // 設置文本的前景色為紅色。
             });
-            displayTextBlock.Inlines.Add(summaryString);
+
             // 將創建的 Italic 物件（即 summaryString）添加到 displayTextBlock 的 Inlines 集合中。
-            // 這樣可以將這段富文本顯示在 displayTextBlock 控件中。
+            //存在UI下面的方框裡
+            displayTextBlock.Inlines.Add(summaryString);
+           
         }
 
         // 將選擇的飲料放入 'orders' 字典
@@ -207,7 +259,8 @@ namespace WpfApp2// 命名空間
             
             myOrders.Clear();// 清空當前的訂單
 
-            // 使用 for 迴圈遍歷 stackpanel_DrinkMenu 的所有子元件。
+            // 使用 for 迴圈遍歷 stackpanel_Dri
+            // nkMenu 的所有子元件。
             // 這通常會是一些 StackPanel，其中包含飲料選項的 CheckBox 和 Slider。
             for (int i = 0; i < stackpanel_DrinkMenu.Children.Count; i++)// 遍歷所有子元件（飲料選項）在 stackpanel_DrinkMenu 中
             {
